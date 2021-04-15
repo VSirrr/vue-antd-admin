77661a3ee397d13f6569bc9a0a78751bc6299435
jianjun.wei <jianjun.wei@qdfae.com>
2021-04-15 19:36:41 +0800

    chore: remove images lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@ module.exports = {
   '*.md': ['prettier --write'],
   'package.json': ['prettier --write'],
   '*.{js,jsx}': ['eslint --fix', 'prettier --write'],
-  '*.{png,jpeg,jpg,gif,svg}': 'imagemin-lint-staged',
   '*.{css,less,html}': ['stylelint --fix', 'prettier --write'],
   '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
