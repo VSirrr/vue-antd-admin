3c768b77fb63a4b19546705e1af553e81ffc82cf
jianjun.wei <jianjun.wei@qdfae.com>
2021-04-16 18:18:40 +0800

    chore: change rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,8 @@
 // https://prettier.io/docs/en/options.html
 module.exports = {
-  printWidth: 80,
   singleQuote: true,
   trailingComma: 'all',
-  jsxBracketSameLine: true,
   proseWrap: 'never',
+  endOfLine: 'lf',
   arrowParens: 'avoid',
 };
