2f8cb436e0fc297d5bde459912cbfa3486632723
jianjun.wei <jianjun.wei@qdfae.com>
2021-04-20 16:12:10 +0800

    refactor: optimize code

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import user from './modules/user';
 import getters from './getters';
 
 Vue.use(Vuex);
 
+// 引入 vuex 模块
+const modules = {};
+const modulesFiles = require.context('./modules', true, /\.js$/);
+
+modulesFiles.keys().forEach(filePath => {
+  const modulesFile = modulesFiles(filePath);
+  const modulesName = filePath.replace(/^\.\/(.*)\.\w+$/, '$1');
+  modules[modulesName] = modulesFile.default;
+});
+
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
-  modules: {
-    user,
-  },
+  modules,
   getters,
 });
