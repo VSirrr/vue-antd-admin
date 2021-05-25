00d2fd5714763235863f50fbba2ff4c9ac48c080
jianjun.wei <jianjun.wei@qdfae.com>
2021-05-25 11:38:55 +0800

    feat: 引入 clipboard 包

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import dragModal from './drag-modal';
+import clipboard from './clipboard';
 
 Vue.directive('drag-modal', dragModal);
+Vue.directive('clipboard', clipboard);
