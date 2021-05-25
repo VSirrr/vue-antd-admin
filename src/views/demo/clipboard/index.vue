a6628d56074e8e325401804048e2fe269c3860db
jianjun.wei <jianjun.wei@qdfae.com>
2021-05-25 11:20:56 +0800

    feat: add page demo/clipboard

---
+++ src/views/demo/clipboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <a-input v-model="copyData" />
+    <a-button
+      v-clipboard:copy="copyData"
+      v-clipboard:success="copySuccess"
+      v-clipboard:error="copyError"
+      type="default"
+    >
+      复制
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Clipboard',
+  data() {
+    return {
+      copyData: 234,
+    };
+  },
+  methods: {
+    copySuccess() {
+      this.$message.success('复制成功');
+    },
+    copyError() {
+      this.$message.error('复制失败');
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
