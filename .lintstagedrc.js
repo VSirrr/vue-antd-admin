1c28d6086485a8e8f1f38f749f4da5b18a3c76f0
jianjun.wei <jianjun.wei@qdfae.com>
2021-06-18 14:51:36 +0800

    chore: pattern rules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   '*.md': ['prettier --write'],
   'package.json': ['prettier --write'],
-  '!(dist/**/*).{js,jsx}': ['eslint --fix', 'prettier --write'],
-  '!(dist/**/*).{css,less,html}': ['stylelint --fix', 'prettier --write'],
-  '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
+  'src/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
+  'src/**/*.{css,less,html}': ['stylelint --fix', 'prettier --write'],
+  'src/**/*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
     'prettier --write--parser json',
   ],
