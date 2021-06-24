e326113048b87287fe6f6ebe24610971d537bc71
jianjun.wei <jianjun.wei@qdfae.com>
2021-06-24 17:29:00 +0800

    feat: add buttons

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,14 @@
 export default {
   // 用户菜单
   routes: state => state.user.routes,
+  // 用户按钮权限集合
+  buttons: state => state.user.buttons,
   // 用户 id
   userId: state => state.user.userInfo.id,
-  // 手机号
+  // 用户手机号
   userPhone: state => state.user.userInfo.phone,
   // 用户名
   userName: state => state.user.userInfo.userName,
-  // 用户角色
+  // 用户角色：1 => 超级管理员 2 => 操作员
   userType: state => state.user.userInfo.userTypeDesc,
 };
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,50 +1,99 @@
 import md5 from 'md5';
-import { setToken, removeToken } from 'utils/token';
+import { removeToken } from '@/utils/token';
 import { login, logout, getOperator } from 'api/operator';
 import router, { resetRouter, adminRoutes, operatorRoutes } from '@/router';
 
+// 对路由表进行筛选
+function generateRoutes(routes, authority) {
+  for (let i = 0; i < routes.length; i++) {
+    const route = routes[i];
+    if (!authority.includes(route.path)) {
+      routes.splice(i, 1);
+      i--;
+      continue;
+    }
+    if (route.children) {
+      generateRoutes(route.children, authority);
+    }
+  }
+}
+
+// 区分菜单和按钮
+function getMenuAndButton(data) {
+  // 数组
+  const menus = [];
+  const buttons = [];
+  data.forEach(item => {
+    if (item.indexOf('menu:') === 0) {
+      menus.push(item.slice(5));
+    } else if (item.indexOf('button:') === 0) {
+      buttons.push(item.slice(7));
+    }
+  });
+  return {
+    menus,
+    buttons,
+  };
+}
+
+// 初始状态
+const initialState = {
+  // 用户菜单
+  routes: [],
+  // 用户按钮权限集合
+  buttons: [],
+  // 用户信息
+  userInfo: {},
+};
+
 export default {
   namespaced: true,
-  state: {
-    // 用户菜单
-    routes: [],
-    // 用户信息
-    userInfo: {},
-  },
+  state: Object.assign({}, initialState),
   mutations: {
     // 处理用户信息
-    dealUserInfo: (state, data) => {
-      if (data.userType === 1) {
-        state.routes = adminRoutes;
-        router.addRoutes(adminRoutes);
+    dealUserInfo: (state, userInfo) => {
+      let routes;
+      if (userInfo.userType === 1) {
+        routes = adminRoutes;
       } else {
-        state.routes = operatorRoutes;
-        router.addRoutes(operatorRoutes);
+        // 区分菜单与按钮
+        const { menus, buttons } = getMenuAndButton(userInfo.authority);
+        // 根据后台返回的菜单过滤路由表
+        generateRoutes(operatorRoutes[0].children, menus);
+        routes = operatorRoutes;
+        // 设置用户按钮权限集合
+        state.buttons = buttons;
       }
-      state.userInfo = data;
+      // 设置用户菜单
+      state.routes = routes;
+      // 设置用户信息
+      state.userInfo = userInfo;
+      // 动态添加路由表
+      router.addRoutes(routes);
     },
     // 清空用户信息
     clearUserInfo: state => {
+      // 移除 token
       removeToken();
+      // 重置路由
       resetRouter();
+      // 重置状态
+      Object.assign(state, initialState);
+      // 重定向到登录页
       router.push('/login');
-      state.routes = [];
-      state.userInfo = {};
     },
   },
   actions: {
     // 登录
     async login({ commit }, { phone, password, pictureCode }) {
       try {
-        const data = await login({
+        const userInfo = await login({
           phone,
-          password: md5(password),
           pictureCode,
+          password: md5(password),
         });
-        // 设置 token
-        setToken(data.userName + data.userType);
-        commit('dealUserInfo', data);
-        return Promise.resolve(data);
+        commit('dealUserInfo', userInfo);
+        return Promise.resolve(userInfo);
       } catch (error) {
         return Promise.reject(error);
       }
@@ -52,9 +101,9 @@ export default {
     // 获取用户信息
     async getUserInfo({ commit }) {
       try {
-        const data = await getOperator();
-        commit('dealUserInfo', data);
-        return Promise.resolve(data);
+        const userInfo = await getOperator();
+        commit('dealUserInfo', userInfo);
+        return Promise.resolve(userInfo);
       } catch (error) {
         return Promise.reject(error);
       }
