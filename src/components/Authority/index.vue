a0066161c40e46962818ed5499fe760c030a0b94
jianjun.wei <jianjun.wei@qdfae.com>
2021-06-24 17:18:24 +0800

    feat: add Authority component

---
+++ src/components/Authority/index.vue
@@ -0,0 +1,23 @@
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Authority',
+  props: {
+    value: {
+      type: String,
+      default: '',
+      reuqired: true,
+    },
+  },
+  computed: {
+    ...mapGetters(['buttons']),
+  },
+  render() {
+    if (this.buttons.includes(this.value)) {
+      return this.$slots.default;
+    }
+    return null;
+  },
+};
+</script>
