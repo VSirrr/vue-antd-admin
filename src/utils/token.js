d85812e8d3c1d1c457494da4958120651bcc2a4e
jianjun.wei <jianjun.wei@qdfae.com>
2021-06-24 17:43:15 +0800

    refactor: use cookie replace storage

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,13 +1,9 @@
-import md5 from 'md5';
-import storage from './storage';
+import cookie from 'js-cookie';
 
 const TOKEN_KEY = 'token';
 
-// 设置 token
-export const setToken = token => storage.set(TOKEN_KEY, md5(token));
-
 // 获取 token
-export const getToken = () => storage.get(TOKEN_KEY);
+export const getToken = () => cookie.get(TOKEN_KEY);
 
 // 移除 token
-export const removeToken = () => storage.remove(TOKEN_KEY);
+export const removeToken = () => cookie.remove(TOKEN_KEY);
