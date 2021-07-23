e49e005893cba5bee8b64f87f9b0d4522114d891
jianjun.wei <jianjun.wei@qdfae.com>
2021-07-23 17:34:17 +0800

    feat: add /demo/amount route

---
+++ src/views/demo/amount/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Amount :value="val" transition />
+  </div>
+</template>
+
+<script>
+import Amount from 'components/Amount';
+
+export default {
+  name: 'AmountDemo',
+  components: {
+    Amount,
+  },
+  data() {
+    return {
+      val: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.val = 1500;
+      setTimeout(() => {
+        this.val = 500;
+      }, 2000);
+    }, 300);
+  },
+};
+</script>
